b25c9e03b32ebbbea79b167a7708ac8976be3c4d
DanielSzarek <daniel.szarek98@gmail.com>
2018-05-22 12:32:17 +0200

    Add scores
    - score.txt
    - 2 functions score write and score show

---
+++ AstroJohn/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.astro = new System.Windows.Forms.PictureBox();
+            this.floor = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.astro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.scoreText.Location = new System.Drawing.Point(20, 20);
+            this.scoreText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(133, 33);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score- 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameEvent);
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.Image = global::WindowsFormsApp1.Properties.Resources.obstacle2;
+            this.obstacle2.Location = new System.Drawing.Point(801, 615);
+            this.obstacle2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(75, 77);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle2.TabIndex = 3;
+            this.obstacle2.TabStop = false;
+            this.obstacle2.Tag = "obstacle";
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.Image = global::WindowsFormsApp1.Properties.Resources.obstacle1;
+            this.obstacle1.Location = new System.Drawing.Point(556, 631);
+            this.obstacle1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(44, 40);
+            this.obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.obstacle1.TabIndex = 2;
+            this.obstacle1.TabStop = false;
+            this.obstacle1.Tag = "obstacle";
+            // 
+            // astro
+            // 
+            this.astro.Image = global::WindowsFormsApp1.Properties.Resources.plyaer_walk_3;
+            this.astro.Location = new System.Drawing.Point(196, 597);
+            this.astro.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.astro.Name = "astro";
+            this.astro.Size = new System.Drawing.Size(66, 92);
+            this.astro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.astro.TabIndex = 1;
+            this.astro.TabStop = false;
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.Color.Black;
+            this.floor.Location = new System.Drawing.Point(-18, 695);
+            this.floor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(978, 77);
+            this.floor.TabIndex = 0;
+            this.floor.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(927, 731);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.obstacle2);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.astro);
+            this.Controls.Add(this.floor);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Astro";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.astro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox astro;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ AstroJohn/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false;
+        int jumpSpeed = 10;
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            resetGame();
+        }
+
+        private void gameEvent(object sender, EventArgs e)
+        {
+            astro.Top += jumpSpeed;
+
+            scoreText.Text = "Score: " + score;
+
+            if (jumping && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed; //przesuwa przeszkody w lewo
+
+                    if(x.Left + x.Width < -120)
+                    {
+                        x.Left = this.ClientSize.Width + rnd.Next(200, 800);
+                        score++;
+                    }
+
+                    if(astro.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+
+                        astro.Image = Properties.Resources.player_static;
+
+                        scoreText.Text += " Press R to restart";
+                    }
+                }
+            }
+
+            if (astro.Top >= 380 && !jumping)
+            {
+                force = 12;
+                astro.Top = floor.Top - astro.Height;
+                jumpSpeed = 0;
+            }
+
+            if (score >= 10)
+            {
+                obstacleSpeed = 15;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space && !jumping)
+            {
+                jumping = true;
+            }
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.R)
+            {
+                resetGame();
+            }
+
+            if(jumping)
+            {
+                jumping = false;
+            }
+
+        }
+
+        private void resetGame()
+        {
+            force = 12;
+            astro.Top = floor.Top - astro.Height;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 10;
+            scoreText.Text = "Score: " + score;
+            astro.Image = Properties.Resources.plyaer_walk_3;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "obstacle")
+                {
+                    int position = rnd.Next(600, 1000);
+
+                    x.Left = 640 + (x.Left + position + x.Width * 3);
+                }
+            }
+
+            gameTimer.Start();
+        }
+
+        private void score_table_show()
+        {
+            using (StreamReader Read = new StreamReader("Scores.txt"))
+            {
+                string line;
+
+                while ((line = Read.ReadLine()) != null)
+                {                    
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        private void score_table_write()
+        {
+            int n = 0;
+            // Wczytywanie wynikow + zapisanie nowego + sortowanie(sortowanie dałbym jako nową klasę, albo funkcje)
+            using (StreamReader Read = new StreamReader("Scores.txt"))
+            {
+                
+                string line;
+                //ilosc lini
+                
+                //Odczytujemy ilosc linii
+                while ((line = Read.ReadLine()) != null)
+                {
+                    n++;                    
+                }
+                string[] lines = new string[n];
+                int[] score = new int[n];
+                
+                int i = 0;
+                //zapisujemy linie do lines,same wyniki do score
+                while (i<n)
+                {
+                    lines[i] = Read.ReadLine();
+                    string[] split = line.Split(' ');
+                    score[i] = int.Parse(split[2]);
+                    i++;
+                }
+                i++;
+                n++;
+                // Dodajemy nowy wynik
+                // Tutaj trzeba bedzie do funkcji wszystko przekazac!!!!!!!!!!!!!!!!!!
+                string nick= "Bartek";
+                int new_score_int = 0;
+                string new_score = (i +" "+ nick +" "+ new_score_int);
+                lines[i] = new_score;
+                
+
+                //------------sortowanie-----------
+                // TRZEBA PRZETESTOWAĆ ZAMIANĘ TABLICY LINES!!!!
+                int k = 0, mini;
+                i = 0;
+                while (k < n - 1)
+                {
+                    k += 1;
+                    mini = k;
+                    i = k;
+                    while (i < n)
+                    {
+                        i++;
+                        if (score[i] < score[mini])
+                        {
+                            mini = i;
+                        }
+                    }
+                    int pom = score[k];
+                    score[k] = score[mini];
+                    score[mini] = pom;
+                    string temp = lines[k];
+                    lines[k] = lines[mini];
+                    lines[mini] = temp;
+                }
+
+                //-------------Zapisywanie do pliku-----------
+                using (StreamWriter write = new StreamWriter("Scores.txt"))
+                {
+                    i=0;
+                    while (i < n)
+                    {
+                        write.Write(lines[i]);
+                        i++;
+                    }
+                }
+            }
+            
+        }
+    }
+}
